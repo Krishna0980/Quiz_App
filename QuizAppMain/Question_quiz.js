f78252942127a28722c372eda9d025dd89c10685
Krishna0908 <krishnarbhatt99@gmail.com>
2021-10-16 22:26:52 +0530

    QuizApp

---
+++ QuizAppMain/Question_quiz.js
@@ -0,0 +1,72 @@
+const Questions = [{
+        no: 1,
+        qus: "What is the profession of Gaitonde’s first father?",
+        a: "Teacher",
+        b: "Gangster",
+        c: "Priest",
+        d: "Painter",
+        ans: "b",
+        note: "right answer is b."
+    },
+    {
+        no: 2,
+        qus: "Who killed Ganesh Gaitonde's mother?",
+        a: "Subhadra",
+        b: "Kanta Bai",
+        c: "Kukoo",
+        d: "Anjali",
+        ans: "b",
+        note: "right answer is b."
+    },
+    {
+        no: 3,
+        qus: "Who killed Ganesh Gaitonde?",
+        a: "Suleiman Isa",
+        b: "Paritosh Shah ",
+        c: "DCP Parulkar ",
+        d: "Ganesh Gaitonde",
+        ans: "a",
+        note: "right answer is a."
+    },
+    {
+        no: 4,
+        qus: "Gareeb logo ki entry sirf mandir mein free hai. Who said this?",
+        a: "Subhadra",
+        b: " Paritosh Shah",
+        c: "Inspector Sartaj Singh",
+        d: "Constable Katekar",
+        ans: "a",
+        note: "right answer is a."
+    },
+    {
+        no: 5,
+        qus: "What was Surveen Chawla’s character’s name in Sacred Games?",
+        a: "Nainika",
+        b: "Jojo ",
+        c: "Anjali ",
+        d: "Kusum Devi Yadav ",
+        ans: "c",
+        note: "right answer is c."
+    },
+    {
+        no: 6,
+        qus: "What is the name of the Bollywood actress living a dual life in Sacred Games?",
+        a: "Naina Singh",
+        b: "Zoya Mirza ",
+        c: "Anjali Mathur",
+        d: "Mikki",
+        ans: "c",
+        note: "right answer is c."
+    },
+    {
+        no: 7,
+        qus: "Who was Gaitonde’s first partner in gold smuggling?",
+        a: "Saleem Kaka",
+        b: "Kanta Bai ",
+        c: "Bunty ",
+        d: "Constable Dilbagh Singh",
+        ans: "d",
+        note: "right answer is d."
+    },
+];
+module.exports = Questions;
